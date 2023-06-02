76bd5a7e606f3e590cf463009f1273710dca2e7f
YellappagariRevathi <131354921+YellappagariRevathi@users.noreply.github.com>
2023-06-02 17:11:38 +0530

    Add files via upload

---
+++ Program202.java
@@ -0,0 +1,14 @@
+import java.until.*;
+public class Program202{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(system.in);
+int n;
+System.out.println("Enter the number:");
+n=sc.nextInt();
+if(n/2*2==n)
+System.out.print("Even\n");
+else
+System.out.print("Odd\n");
+}
+}
